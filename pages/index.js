6686b2762e73ecfa33528321a041ab92cf821bbf
Rodrigo Prado <88177581+rpfigueiredo@users.noreply.github.com>
2025-02-13 02:18:08 +0000

    minha casa

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-export default function Home(){
-    return <h1>Olá mundo!</h1>
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Abaixo tenho a minha casa!</h1>
+      <Image
+        src="/casa.webp"  
+        width={500}
+        height={500}
+        alt="Imagem de uma casa"
+      />
+    </div>
+  );
 }
